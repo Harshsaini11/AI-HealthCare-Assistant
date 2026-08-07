eb6df8df6c0629102400a5907b13ada13302e175
HARSH KUMAR SAINI <144931307+Harshsaini11@users.noreply.github.com>
2026-08-07 21:16:04 +0530

    Update app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://127.0.0.1:8000/api/analyze';
+const API_URL = 'https://ai-healthcare-assistant-backend-kgb9.onrender.com/api/analyze';
 
 // Master Disease Database
 const symptomData = {
