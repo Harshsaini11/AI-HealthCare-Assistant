57f618da431a928fdd723a6fb2017f21153230c2
HARSH KUMAR SAINI <144931307+Harshsaini11@users.noreply.github.com>
2026-08-08 18:27:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -172,4 +172,5 @@ app.post('/api/symptoms', async (req, res) => {
     }
 });
 
-app.listen(8000, () => console.log('Node.js Server running on port 8000'));
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Node.js Server running on port ${PORT}`));
